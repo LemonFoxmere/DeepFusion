3722cac9bcacc3bda5f152c0f43c5db8ca406470
DeliciousLlama <248266816j@gmail.com>
2021-08-25 10:18:01 -0700

    added buttons and menu demos

---
+++ model-maker/edge-render.js
@@ -0,0 +1,70 @@
+dragElement(document.getElementById('destnode')); // add element drag
+
+let data = fetch('./test-template.json').then((res) => {
+    console.log(res)
+})
+
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+function createNodeElement() {
+    let node = document.createElement("div");
+    // create unique uuid
+    let uuid = uuidv4()
+    node.id = uuid
+
+    return node;
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        document.querySelectorAll('.node').forEach(elmnt => {
+            elmnt.classList.add('notransition')
+        });
+
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2/zoom) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1/zoom) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+        // add back transition
+        document.querySelectorAll('.node').forEach(elmnt => {
+            elmnt.classList.remove('notransition')
+        });
+    }
+}
--- model-maker/events.js
+++ model-maker/events.js
@@ -1,6 +1,5 @@
 // Make the DIV element draggable:
-dragElement(document.getElementById('00x0')); // add element drag
-dragElement(document.getElementById('00x1')); // add element drag
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 let canvas_position_x = 0;
 let canvas_position_y = 0;
@@ -10,53 +9,12 @@ let zoom = 1;
 const WHEEL_ZOOM_SPEED = 0.1;
 const CLICK_ZOOM_SPEED = 0.25;
 
-dragCanvas(document.querySelector('#main-canvas'), '.node'); // add 
+dragCanvas(document.querySelector('#canvas-drag'), '.node'); // add 
 let shifted = false;
-
-function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-        // if present, the header is where you move the DIV from:
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-        // otherwise, move the DIV from anywhere inside the DIV:
-        elmnt.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-
-    }
-
-    function elementDrag(e) {
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2/zoom) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1/zoom) + "px";
-    }
-
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
 
 function dragCanvas(canvas, elmnts) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    document.onmousedown = dragMouseDown;
+    canvas.onmousedown = dragMouseDown;
 
     function dragMouseDown(e) {
         e.preventDefault();
@@ -64,31 +22,35 @@ function dragCanvas(canvas, elmnts) {
         pos3 = e.clientX;
         pos4 = e.clientY;
 
-        // check if its inside of any node / menus
+            // check if its inside of any node / menus
         let x = false;
         document.querySelectorAll(elmnts).forEach(elmnt => {
             let box = elmnt.getBoundingClientRect()
             if(pos3 > box.x && pos3 < box.x + box.width &&
                 pos4 > box.y && pos4 < box.y + box.height){
                 x = true;
-                return;
             }
         });
+
+        if(x) return
+        document.querySelectorAll('.node-drag').forEach((elm2) => {
+            elm2.classList.remove('selected-node')
+        }) // remove all node selection
 
         document.querySelectorAll('.menu-container').forEach(elmnt => {
             let box = elmnt.getBoundingClientRect()
             if(pos3 > box.x && pos3 < box.x + box.width &&
                 pos4 > box.y && pos4 < box.y + box.height){
                 x = true;
-                return;
             }
         });
 
-        console.log(x)
+        if(x) return
 
-        if(x){
-            return
-        }
+        // remove transition temporarily
+        document.querySelectorAll(elmnts).forEach(elmnt => {
+            elmnt.classList.add('notransition')
+        });
 
         document.onmouseup = closeDragElement;
         // call a function whenever the cursor moves:
@@ -111,31 +73,70 @@ function dragCanvas(canvas, elmnts) {
         document.querySelector('.crosshair').style.top = (document.querySelector('.crosshair').offsetTop - pos2) + "px";
         document.querySelector('.crosshair').style.left = (document.querySelector('.crosshair').offsetLeft - pos1) + "px";
         
-        canvas_position_x -= pos1; //it's the opposite way around
-        canvas_position_y -= pos2; //it's the opposite way around
+        canvas_position_x -= pos1/zoom; //it's the opposite way around
+        canvas_position_y -= pos2/zoom; //it's the opposite way around
         // get string and apply
-        document.querySelector('#position_debug').innerHTML = canvas_position_x + "," + canvas_position_y
+        document.querySelector('#position_debug').innerHTML = Math.floor(canvas_position_x) + "," + Math.floor(canvas_position_y)
     }
 
     function closeDragElement() {
         // stop moving when mouse button is released:
         document.onmouseup = null;
         document.onmousemove = null;
+        
+        // add back no ransition
+        document.querySelectorAll(elmnts).forEach(elmnt => {
+            elmnt.classList.remove('notransition')
+        });
     }
 }
 
+// add selection highlight
+document.querySelectorAll('.node-drag').forEach((elm) => {
+    elm.addEventListener('mousedown', (e) => {
+        if(elm.classList.contains('selected-node')){
+            elm.classList.remove('selected-node')
+            return
+        }
+        document.querySelectorAll('.node-drag').forEach((elm2) => {
+            elm2.classList.remove('selected-node')
+        })
+        elm.classList.add('selected-node')
+    })
+})
+
+// reset canvas position
+document.getElementById('reset-canvas').addEventListener('click', async (e) => {
+    document.querySelectorAll('.node').forEach((e) => {
+        e.classList.remove('notransition')
+    })
+
+    document.querySelectorAll('.node').forEach((e) => {
+        e.style.top = (e.offsetTop - canvas_position_y) + "px";
+        e.style.left = (e.offsetLeft - canvas_position_x) + "px";
+    })
+    
+    canvas_position_y = 0
+    canvas_position_x = 0
+    document.querySelector('#position_debug').innerHTML = canvas_position_x + "," + canvas_position_y
+})
+
 // add zoom functionality
 document.getElementById('zoom-in').addEventListener('click', (e) => {
-    if(zoom < 3){
-        canvas.style.transform = `scale(${zoom += CLICK_ZOOM_SPEED})`;  
+    zoom += CLICK_ZOOM_SPEED
+    if(zoom > 3){
+        zoom = 3        
     }
+    canvas.style.transform = `scale(${zoom})`;  
     update_zoom_text()
 })
 
 document.getElementById('zoom-out').addEventListener('click', (e) => {
-    if(zoom > 0.25){
-        canvas.style.transform = `scale(${zoom -= CLICK_ZOOM_SPEED})`;  
+    zoom -= CLICK_ZOOM_SPEED
+    if(zoom < 0.2){
+        zoom = 0.2        
     }
+    canvas.style.transform = `scale(${zoom})`;  
     update_zoom_text()
 })
 
@@ -159,3 +160,47 @@ document.addEventListener("wheel", function(e) {
 function update_zoom_text(){
     document.getElementById('zoom-scale').innerHTML = Math.floor(zoom*100) + "%"
 }
+
+document.querySelectorAll(".slider").forEach((e) => {
+    e.onmousedown = (evt) => {
+        e.onmousemove = (evt2) => {
+            if(document.getElementById(e.id.substring(0,e.id.length-7))){
+                document.getElementById(e.id.substring(0,e.id.length-7)).value = e.value;
+            }
+        }
+    }
+    e.onmouseup = (evt) => {
+        e.onmousemove = null
+    }
+})
+
+// field input related
+document.querySelectorAll(".number-box").forEach((e) => {
+    e.addEventListener("keypress", function (evt) {
+        if (evt.which < 48 || evt.which > 57){
+            evt.preventDefault();
+        }
+
+        if (evt.which === 13){
+            e.blur()
+        }
+    }); // prevent unorthodox number entering
+    
+    e.addEventListener("focusout", (evt) => {
+        if(document.getElementById(e.id + '-slider')){
+            let slider = document.getElementById(e.id + '-slider')
+            let max_val = slider.max
+            let min_val = slider.min
+            if (Number(e.value) > max_val){
+                e.value = max_val;
+            } else if (Number(e.value) < min_val){
+                e.value = min_val;
+            }
+            slider.value = e.value
+        }
+    })
+
+    if(document.getElementById(e.id + '-slider')){
+        e.value = document.getElementById(e.id + '-slider').value;
+    }
+})
